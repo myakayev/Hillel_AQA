47c31557a813366ebfa623c7f3d094e577d24670
myakayev <48683938+myakayev@users.noreply.github.com>
2023-10-24 23:51:00 +0300

    homework_3. Almost finish last task.

--- src/main/java/homework_3/SalaryCalculation.java
+++ src/main/java/homework_3/SalaryCalculation.java
@@ -1,6 +1,7 @@
 package homework_3;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class SalaryCalculation {
     int hours = 8;
@@ -8,43 +9,57 @@ public class SalaryCalculation {
     int[] month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     public static void main(String[] args) {
-        double[] arr = new double[args.length];
-        try {
-            for (int i = 0; i < args.length; i++) {
-                arr[i] = Double.parseDouble(args[i]);
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("Enter numbers, please!");
-        }
+        BigDecimal hourlyCost = new BigDecimal(args[0]);
+        BigDecimal taxRate = new BigDecimal(args[1]);
+        System.out.println(Arrays.toString(workingDaysForEachMonth()));
+        //  System.out.println("Salary for month without taxes: " + salaryPerMonth(workingDays(month),daySalary(hourlyCost)));
+        // System.out.println("Salary for month with taxes: " + salaryPerMonthWithTaxes(workingDays(month),daySalaryWithTaxes(hourlyCost,taxRate)));
 
     }
-    static BigDecimal daySalary (BigDecimal hourlyCost) {
+
+    static BigDecimal daySalary(BigDecimal hourlyCost) {
         return hourlyCost.multiply(BigDecimal.valueOf(8.0));
     }
 
-    static BigDecimal daySalaryWithTaxes (BigDecimal hourlyCost, BigDecimal taxRate) {
+    static BigDecimal daySalaryWithTaxes(BigDecimal hourlyCost, BigDecimal taxRate) {
         BigDecimal salaryPerDay = hourlyCost.multiply(BigDecimal.valueOf(8.0));
         BigDecimal taxAmount = salaryPerDay.multiply(taxRate).multiply(BigDecimal.valueOf(0.01));
         return salaryPerDay.subtract(taxAmount);
     }
-    static BigDecimal salaryPerMonth (int workingDays, BigDecimal daySalary) {
-        return daySalary.multiply(BigDecimal.valueOf(workingDays));
-    }
 
-    static BigDecimal salaryPerMonthWithTaxes (int workingDays, BigDecimal daySalaryWithTaxes) {
-        return daySalaryWithTaxes.multiply(BigDecimal.valueOf(workingDays));
-    }
 
-    static int workingDays (int month) {
-        int workingDays = 0;
-        int k = 0;
+    static int[] workingDaysForEachMonth() {
         int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        for (int i = 0; i < months[month - 1]; i++) {
-            k++;
-            if (k <= 5) workingDays++;
-            if (k == 7) k = 0;
+        int[] workingDaysForEachMonth = new int[12];
+        int k = 0;
+        for (int i = 0; i < 12 ; i++) {
+            int workingDays = 0;
+            for (int j = 0; j < months[i] ; j++) {
+                k++;
+                if (k <= 5) workingDays++;
+                if (k == 7) k = 0;
+            }
+            workingDaysForEachMonth[i] = workingDays;
         }
-        return workingDays;
+        return workingDaysForEachMonth;
+    }
+
+    static BigDecimal[] salaryForEachMonth(int[] workingDaysForEachMonth, BigDecimal daySalary) {
+        BigDecimal[] salaryForEachMonth = new BigDecimal[workingDaysForEachMonth.length];
+        for (int i = 0; i < workingDaysForEachMonth.length; i++) {
+            salaryForEachMonth[i] = daySalary.multiply(BigDecimal.valueOf(workingDaysForEachMonth[i]));
+
+        }
+        return salaryForEachMonth;
+    }
+
+    static  BigDecimal[] salaryForEachMonthWithTaxes(int[] workingDaysForEachMonth, BigDecimal daySalaryWithTaxes) {
+        BigDecimal[] salaryForEachMonth = new BigDecimal[workingDaysForEachMonth.length];
+        for (int i = 0; i < workingDaysForEachMonth.length; i++) {
+            salaryForEachMonth[i] = daySalaryWithTaxes.multiply(BigDecimal.valueOf(workingDaysForEachMonth[i]));
+
+        }
+        return salaryForEachMonth;
     }
 
 }
