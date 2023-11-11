085c16fe5c25a8f8c2261b625b48fc627e781401
myakayev <48683938+myakayev@users.noreply.github.com>
2023-11-11 23:57:25 +0200

    homework 8. penguin swimming

--- src/main/java/homework_8/birds/non_flying_birds/impl/Duck.java
+++ src/main/java/homework_8/birds/non_flying_birds/impl/Duck.java
@@ -1,8 +1,9 @@
 package homework_8.birds.non_flying_birds.impl;
 
+import homework_8.Swimable;
 import homework_8.birds.non_flying_birds.NonFlyableBird;
 
-public class Duck extends NonFlyableBird {
+public class Duck extends NonFlyableBird implements Swimable {
     public Duck(String name, String color) {
         super(name, color);
     }
@@ -10,5 +11,10 @@ public class Duck extends NonFlyableBird {
     @Override
     protected void voice() {
         System.out.println("Quack-quack!");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("I can swim like duck!");
     }
 }
