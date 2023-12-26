3b2b8843509b562589937fd6c48abbe45dc2012b
myakayev <48683938+myakayev@users.noreply.github.com>
2023-12-26 08:30:44 +0200

    homework 12. Begin

---
+++ src/main/java/homework_12/classwork_23/TestingCoursePage.java
@@ -0,0 +1,19 @@
+package homework_12.classwork_23;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class TestingCoursePage {
+    private WebDriver driver;
+
+    private WebElement qaManualBlock;
+    private WebElement qaAutomationBlock;
+    private WebElement qaAdditionalBlock;
+
+    private List<String> getCourses() {
+        return null;
+    }
+
+}
